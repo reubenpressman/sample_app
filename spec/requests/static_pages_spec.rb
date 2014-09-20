30989651adc877462526c6633b58fc461dad6a29
Reuben Pressman <talk@reubenpressman.com>
2014-09-20 17:21:49 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,18 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
 
   end
@@ -20,7 +24,6 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
@@ -35,7 +38,6 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
