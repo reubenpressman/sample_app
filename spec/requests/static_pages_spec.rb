d029cefadc90791441066fa8ef85331a3479a6fa
Reuben Pressman <talk@reubenpressman.com>
2014-09-26 01:06:13 -0400

    start 5.3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title" do
-    	visit '/static_pages/home'
+    	visit root_path
     	expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
 
@@ -24,12 +24,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-    	visit '/static_pages/help'
+    	visit help_path
     	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
 
@@ -38,12 +38,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-    	visit '/static_pages/about'
+    	visit about_path
     	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
 
@@ -52,12 +52,12 @@ describe "Static pages" do
   describe "Contact Page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
 
