1b40a0f3bd21dd4d54cb6f6544a8982975d0849b
Reuben Pressman <talk@reubenpressman.com>
2014-12-04 22:40:39 -0500

    start of 7.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,17 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
   		#yay!
   	else
   		render 'new'
   	end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
